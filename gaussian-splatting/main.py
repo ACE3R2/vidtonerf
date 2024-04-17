69c53b3677c29bd74977faad1795441ce65eb3aa
ACE3R2 <bavrahamics@gmail.com>
2024-04-17 18:45:42 -0400

    Created skeleton of consumption under overall program

--- gaussian-splatting/main.py
+++ gaussian-splatting/main.py
@@ -12,6 +12,14 @@ from utils.image_utils import psnr
 from argparse import ArgumentParser, Namespace
 from arguments import ModelParams, PipelineParams, OptimizationParams
 from train import training,training_report
+import requests
+import pika
+import json
+import time
+from multiprocessing import Process
+import os
+import argparse
+import sys
 try:
     from torch.utils.tensorboard import SummaryWriter
     TENSORBOARD_FOUND = True
@@ -20,12 +28,39 @@ except ImportError:
 
 
 
+def splatting_worker(args):
+    rabbitmq_domain = "rabbitmq"
+    credentials = pika.PlainCredentials(str(os.getenv("RABBITMQ_DEFAULT_USER")), str(os.getenv("RABBITMQ_DEFAULT_PASS")))
+    parameters = pika.ConnectionParameters(
+        rabbitmq_domain, 5672, "/", credentials, heartbeat=300
+    )
+
+    connection = pika.BlockingConnection(parameters)
+    channel = connection.channel()
+    channel.queue_declare(queue="nerf-in")
+    channel.queue_declare(queue="nerf-out")
+
+    def process_nerf_job(ch, method, properties, body):
+        print("Entered consumption",flush=True)
+
+        torch.autograd.set_detect_anomaly(args.detect_anomaly)
+        training(lp.extract(args), op.extract(args), pp.extract(args), args.test_iterations, args.save_iterations, args.checkpoint_iterations, args.start_checkpoint, args.debug_from)
+
+        print("\nTraining complete.",flush=True)
+        ch.basic_ack(delivery_tag=method.delivery_tag)
+
+
+    channel.basic_qos(prefetch_count=1)
+    channel.basic_consume(queue="nerf-in", on_message_callback=process_nerf_job)
+    channel.start_consuming()
 
 
 
 
 
-if __name__ == "__main__":
+
+
+def set_up_arguments():
     # Set up command line argument parser
     parser = ArgumentParser(description="Training script parameters")
     lp = ModelParams(parser)
@@ -42,16 +77,13 @@ if __name__ == "__main__":
     parser.add_argument("--start_checkpoint", type=str, default = None)
     args = parser.parse_args(sys.argv[1:])
     args.save_iterations.append(args.iterations)
-
     print("Optimizing " + args.model_path,flush=True)
-
     # Initialize system state (RNG)
     safe_state(args.quiet)
 
-    # Start GUI server, configure and run training
-    network_gui.init(args.ip, args.port)
-    torch.autograd.set_detect_anomaly(args.detect_anomaly)
-    training(lp.extract(args), op.extract(args), pp.extract(args), args.test_iterations, args.save_iterations, args.checkpoint_iterations, args.start_checkpoint, args.debug_from)
 
-    # All done
-    print("\nTraining complete.",flush=True)
+if __name__ == "__main__":
+
+    cmdlineargs = set_up_arguments()
+
+    nerfprocess = Process(splatting_worker, args=cmdlineargs)
